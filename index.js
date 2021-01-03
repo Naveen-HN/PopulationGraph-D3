9c0eda6592eaf13841550972e5bf4f8287bbc6be
Naveen-HN <hnnaveen93@gmail.com>
2021-01-03 12:30:41 -0500

    added scatter plot

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const scaleBand = d3.scaleBand;
 const axisLeft = d3.axisLeft;
 const axisBottom = d3.axisBottom;
 const format = d3.format;
+const scalePoint = d3.scalePoint;
 const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
@@ -16,24 +17,27 @@ const render = data => {
     const yValue = d => d.country;
     const margin = {
         top: 50,
-        right: 20,
+        right: 30,
         bottom: 70,
         left: 200
     }
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
-    const xScale = scaleLinear().domain([0, max(data, d => xValue(d))]).range([0, innerWidth]);
+    const xScale = scaleLinear().domain([0, max(data, d => xValue(d))]).range([0, innerWidth]).nice();
 
     const yScale = scaleBand().domain(data.map(d => yValue(d))).range([0, innerHeight]).padding(0.1);
     console.log(yScale.domain())
 
+    const cyScale = scalePoint().domain(data.map(d => yValue(d))).range([0, innerHeight]).padding(0.5);
 
     const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
 
     const xAxisTickFormat = number => format('.3s')(number).replace('G', 'B');
     const xAxis = axisBottom(xScale).tickFormat(xAxisTickFormat).tickSize(-innerHeight);
-    g.append('g').call(axisLeft(yScale)).selectAll('.domain, .tick line').remove();
+
+    const yAxis = axisLeft(yScale).tickSize(-innerWidth)
+    g.append('g').call(yAxis).select('.domain').remove();
     const xAxisG = g.append('g').call(xAxis).attr('transform', `translate(0,${innerHeight})`);
 
     xAxisG.select('.domain').remove();
@@ -46,9 +50,17 @@ const render = data => {
         .attr('width', d => xScale(xValue(d)))
         .attr('height', yScale.bandwidth())
         .attr('fill', 'steelblue')
-
-
     g.append('text').attr('class', 'title').attr('y', -10).text('Top 14 most populous countries in the world')
+
+
+    g.selectAll('circle').data(data)
+        .enter().append('circle')
+        .attr('cy', d => cyScale(yValue(d)))
+        .attr('cx', d => xScale(xValue(d)))
+        .attr('r', 10)
+        .attr('fill', '#A93226')
+
+
 }
 
 
